41b262bdbef40714b7c5ccec830d8dbb6b5616ea
djamelall22 <alphamodeling.contact@gmail.com>
2020-02-29 18:17:08 +0100

    get categories added

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,7 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -25,7 +26,20 @@ def create_app(test_config=None):
                            'GET, PATCH, POST, DELETE, OPTIONS')
       return response
 
-  
+  @app.route('/categories', methods=['GET'])
+  def get_all_categories():
+      """
+      Creates a dictionary of all categories
+      :return: All categories
+      """
+      categories = {}
+      for category in Category.query.all():
+          categories[category.id] = category.type
+      return jsonify({
+          'categories': categories
+      })
+
+
   return app
 
 
